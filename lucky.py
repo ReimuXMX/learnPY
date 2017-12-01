3c0b0be41275511a6d9b80fbd6cc9f6c0a902abf
魔仙灵梦 <balalaxmxtlm@outlook.com>
2017-12-01 09:03:30 +0800

    Add lucky.py

---
+++ lucky.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+import random
+import time
+
+
+def lucky_roll():
+    num_player = int(input('Please enter a number between 1 - 10: '))
+    if num_player > 10:
+        print('ERROR')
+        exit(1)
+
+    print('AI Rolling...')
+    time.sleep(2)
+    num_ai = random.randint(1, 10)
+    print('AI has rolled ' + str(num_ai))
+
+    if num_player > num_ai:
+        print('LUCKY!')
+    elif num_player < num_ai:
+        print('UNLUCKY...QAQ')
+    else:
+        pass
+
+
+def lucky_end():
+    num_opt = input('Quit? (Y/N) ')
+
+    if num_opt == 'Y' or num_opt == 'y':
+        exit()
+    elif num_opt == 'N' or num_opt == 'n':
+        lucky_roll()
+    else:
+        print('Please enter Y or N!')
+        return 0
+
+
+def lucky_main():
+    lucky_roll()
+    while True:
+        lucky_end()
+
+
+lucky_main()
